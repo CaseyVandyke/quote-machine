94f942823e263412cd54825e72bb965d74236655
CaseyVandyke <caseyvandyke2@gmail.com>
2024-10-09 14:18:11 -0600

    random quote generator complete

--- quote.js
+++ quote.js
@@ -1,8 +1,24 @@
 const quotes = [
-    { quote: "Life is like a box of chocolates", author: "Forest Gump" },
-    { quote: "quote 2", author: "author 2" },
-    { quote: "quote 3", author: "author 3" },
-    { quote: "quote 4", author: "author 4" }
-]
+    {
+        quote: "“Life is like a box of chocolates. You never know what you're gonna get.”",
+        author: "- Forrest Gump",
+        color: '#0D98BA'
+    },
+    {
+        quote: "“Be yourself; everyone else is already taken.”",
+        author: "- Oscar Wilde",
+        color: '#e74d3d'
+    },
+    {
+        quote: "“So many books, so little time.”",
+        author: "- Frank Zappa",
+        color: '#2d3e51'
+    },
+    {
+        quote: "“A room without books is like a body without a soul.”",
+        author: "- Marcus Tullius Cicero",
+        color: '#f29d12'
+    }
+];
 
 export default quotes;
--- script.js
+++ script.js
@@ -1,10 +1,27 @@
 import quotes from './quote.js';
 const quoteText = document.querySelector("#text");
+const author = document.querySelector("#author");
+const quoteContainer = document.querySelector("#quote-box");
+const quoteButton = document.querySelector("#new-quote");
 
-const randomQuote = Math.floor(Math.random() * quotes.length);
+function onLoad() {
+    const randomQuote = Math.floor(Math.random() * quotes.length);
+    const { quote, author: quoteAuthor, color } = quotes[randomQuote];
+    const body = document.body.style;
+    const button = quoteButton.style;
+    const animate = 'background-color 0.5s ease';
 
-quoteText.innerHTML += `${quotes[randomQuote].quote}`
+    quoteText.innerHTML = `${quote}`;
+    author.innerHTML = `${quoteAuthor}`;
+    body.transition = animate;
+    body.backgroundColor = color;
+    body.color = color;
+    button.backgroundColor = color;
+    button.transition = animate;
+}
 
+quoteButton.addEventListener('click', () => {
+    onLoad();
+});
 
-
-
+onLoad();
